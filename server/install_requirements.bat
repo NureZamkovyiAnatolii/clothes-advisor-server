771123667e63f606674515c0fd349e583ac6807e
Zamkovyi Anatolii <anatolii.zamkovyi@nure.ua>
2025-04-30 12:07:02 +0300

    - Added virtual env for project.

--- server/install_requirements.bat
+++ server/install_requirements.bat
@@ -1,6 +1,9 @@
 @echo off
+echo Activating virtual environment...
+call venv\Scripts\activate
+
 echo Installing dependencies...
 pip install -r requirements.txt
 echo Installation completed.
-pause
 
+pause
--- server/run_server.bat
+++ server/run_server.bat
@@ -1,9 +1,11 @@
 @echo off
+echo Activating virtual environment...
+call venv\Scripts\activate
 
 echo Starting FastAPI server...
 python -m app.seeding_manager.seed
-echo  http://127.0.0.1:8000/docs
-uvicorn app.main:app 
+
+echo Server running at http://127.0.0.1:8000/docs
+uvicorn app.main:app --reload
+
 pause
-
-
